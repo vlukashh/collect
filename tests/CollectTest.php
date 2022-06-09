f4fb9e1d7d8ef7d8ac8bc18e5313bb7ff2c60cd1
jurapro <jurapro@yandex.ru>
2022-06-09 12:53:15 +0700

    add demo tests

---
+++ tests/CollectTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CollectTest extends TestCase
+{
+    public function testCount()
+    {
+        $collect = new Collect\Collect([13,17]);
+        $this->assertSame(2, $collect->count());
+    }
+
+}
