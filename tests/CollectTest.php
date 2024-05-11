75ec50a07c14249880ea10f36cc70b06f338b845
vlukashh <150641455+vlukashh@users.noreply.github.com>
2024-05-11 13:41:57 +0700

    изменения

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -30,55 +30,6 @@ class CollectTest extends TestCase
         $result = (new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]))->except('b')->toArray();
         $this->assertEquals(['a' => 1, 'c' => 3], $result);
     }
-    public function testOnly()
-    {
-        $result = (new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]))->only('a', 'c')->toArray();
-        $this->assertEquals(['a' => 1, 'c' => 3], $result);
-    }
-    public function testFirst()
-    {
-        $result = (new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]))->first();
-        $this->assertEquals(1, $result);
-    }
-    public function testCountM()
-    {
-        $result = (new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]))->count();
-        $this->assertEquals(3, $result);
-    }
-    public function testToArray()
-    {
-        $result = (new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]))->toArray();
-        $this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3], $result);
-    }
-    public function testPush()
-    {
-        $result = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result->push(4, 'd');
-        $this->assertEquals(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4], $result->toArray());
-    }
-    public function testUnshift()
-    {
-        $result = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result->unshift(0);
-        $this->assertEquals([0, 'a' => 1, 'b' => 2, 'c' => 3], $result->toArray());
-    }
-    public function testShift()
-    {
-        $result = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result->shift();
-        $this->assertEquals(['b' => 2, 'c' => 3], $result->toArray());
-    }
-    public function testPop()
-    {
-        $result = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result->pop();
-        $this->assertEquals(['a' => 1, 'b' => 2], $result->toArray());
-    }
-    public function testSplice()
-    {
-        $result = new Collect\Collect(['a' => 1, 'b' => 2, 'c' => 3]);
-        $result->splice(['a' => 1, 'b' => 2, 'c' => 3], 1, 1);
-        $this->assertEquals(['b' => 2, 'c' => 3], $result->toArray());
-    }
+    
 
 }
